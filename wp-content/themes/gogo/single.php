193b9d7622a47adc6beeca38c061175330d8d748
Marcin <marcin.adam.mroczko@gmail.com>
2021-01-30 12:23:49 +0100

    query posts

--- wp-content/themes/gogo/single.php
+++ wp-content/themes/gogo/single.php
@@ -3,9 +3,11 @@
   <section class="section">
     <h1><?php the_title(); ?></h1>
   </section>
-  <section class="content theme theme--light">
-    <div class="content__inner">
-      <?php the_content(); ?>
+  <section class="section">
+    <div class="container">
+      <div class="container__inner container__inner--subpage">
+        <?php the_content(); ?>
+      </div>
     </div>
   </section>
 </article>
--- wp-content/themes/gogo/template_parts/container.php
+++ wp-content/themes/gogo/template_parts/container.php
@@ -1,16 +1,16 @@
 <?php 
-$args = array( 'post_type' => 'example-cpt', 'posts_per_page' => 10 );
+$args = array( 'posts_per_page' => 10 );
 $the_query = new WP_Query( $args ); ?>
 <?php if ( $the_query->have_posts() ) : ?>
 <div class="container">
-  <h2>Heading</h2>
+  <?php if(get_field('subheading')) : ?><h2><?php echo the_field('subheading'); ?></h2><?php endif; ?>
   <div class="container__inner">
     <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-    <div class="box">
+    <a href="<?php the_permalink(); ?>" class="box box--link">
       <?php the_title('<h3 class="box__heading">', '</h3>'); ?>
       <?php if(has_excerpt()) : ?><p class="box__desc"><?php echo get_the_excerpt(); ?></p><?php endif; ?>
       <?php if (has_post_thumbnail()) : ?><div class="box__ic"><?php the_post_thumbnail(); ?></div><?php endif; ?>
-    </div>
+      </a>
     <?php endwhile;
     wp_reset_postdata(); ?>
   </div>  
