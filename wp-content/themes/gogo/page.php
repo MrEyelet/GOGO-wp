18c55e08afd99a6cffc969452bced433bb3eca53
Marcin <marcin.adam.mroczko@gmail.com>
2021-02-02 09:15:12 +0100

    page default

--- wp-content/themes/gogo/page.php
+++ wp-content/themes/gogo/page.php
@@ -1,5 +1,18 @@
 <?php get_header(); ?>
 
-<!--default page -->
+<article class="theme theme--light">
+  <section class="section">
+    <h1 class="section__heading"><?php the_title(); ?></h1>
+  </section>
+  <?php if( '' !== get_post()->post_content ) : ?>
+  <section class="section">
+    <div class="container">
+      <div class="container__inner container__inner--subpage">
+        <?php the_content(); ?>
+      </div>
+    </div>
+  </section>
+  <?php endif; ?>
+</article>
 
 <?php get_footer();?>
